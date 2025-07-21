d4a1293cb43b7476ca75a428d43b52777d258b1b
JosephWest2 <joeywest1414@gmail.com>
2025-07-20 23:41:35 -0600

    updates

--- pages/index/index.go
+++ pages/index/index.go
@@ -2,9 +2,10 @@ package index
 
 import (
 	"net/http"
+
 	"josephwest2.com/portfolio/pages"
 )
 
 func Get(w http.ResponseWriter, r *http.Request) {
-    pages.Render(index(), w)
+	pages.Render(index(), w)
 }
